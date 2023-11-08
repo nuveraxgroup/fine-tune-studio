2556663ab4edb7d002bda0c292470ce0631fd21c
Luis Hernandez <lfernando2091@gmail.com>
2023-11-07 23:02:10 -0600

    working

--- frontend/studio/src/app/pages/jsonl-overview/tabs/JsonOverviewTabs.tsx
+++ frontend/studio/src/app/pages/jsonl-overview/tabs/JsonOverviewTabs.tsx
@@ -6,6 +6,10 @@ const Details =
   lazy(() => import('./details/DetailsTab')
     .then(({ DetailsTab }) => ({ default: DetailsTab })))
 
+const Samples =
+  lazy(() => import('./samples/SamplesTab')
+    .then(({ SamplesTab }) => ({ default: SamplesTab })))
+
 export const JsonOverviewTabs = () => {
   const [tab, setTab] = useState(0)
   const onChangeTab = (event: SyntheticEvent, newValue: number) => {
@@ -15,10 +19,14 @@ export const JsonOverviewTabs = () => {
     <Box sx={{ fontSize: "10px", borderBottom: 1, borderColor: 'divider' }}>
       <Tabs value={tab} onChange={onChangeTab}>
         <Tab label="Details" {...a11yProps(0)}/>
+        <Tab label="Samples" {...a11yProps(1)}/>
       </Tabs>
     </Box>
     <SuspenseTabPanel value={tab} index={0}>
       <Details/>
+    </SuspenseTabPanel>
+    <SuspenseTabPanel value={tab} index={1}>
+      <Samples/>
     </SuspenseTabPanel>
   </>)
 }
